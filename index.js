9600c123b913637697cd6452f2d5298d85d0806e
dmartinez82 <71946169+dmartinez82@users.noreply.github.com>
2022-03-02 13:26:36 +0100

    Commit inicial: enunciat

---
+++ index.js
@@ -0,0 +1,15 @@
+function _id(ident){
+    return document.getElementById(ident);
+}
+
+function calculaHora(){
+    
+}
+
+function calculaCapital(){
+    
+}
+
+function canviaEstil(tipus){
+    
+}
