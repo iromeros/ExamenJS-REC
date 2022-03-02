38f62b2256243c2fadc8bcc772a7729f8296bcb3
ivan <iromerosanchez9@gmail.com>
2022-03-02 19:40:32 +0100

    commit

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-function _id(ident){
+function _id(ident) {
     return document.getElementById(ident);
 }
 
-function calculaHora(){
+function calculaHora() {
+    var inputTime = _id("inputTime").value;
+    var horayminutos = inputTime.split(":");
+    var h = horayminutos[0]
+    var m = horayminutos[1]
+    let resultatTime;
+    if (inputTime = "m") {
+        h = "m" / 60
+    }
+    else 
+        resultatTime = ((h * 60) + m) * 60
     
+
 }
 
-function calculaCapital(){
-    
+function calculaCapital() {
+    let capitalInicial = _id("capitalInicial").value;
+    let capitalInicial = parseFloat(_id("capitalInicial").value);
+
+    let number = temps / (3 * number / 100);
+    let number = temps - capitalFinal;
+    _id("number").innerHTML = number.toFixed(2);
+    _id("temps").innerHTML = temps.toFixed(2);
 }
 
-function canviaEstil(tipus){
+
+function canviaEstil(tipus) {
+    var simbolos, color;
+        simbolos = "0123456789ABCDEF";
+        color = "#";
     
+        for(var i = 0; i < 6; i++){
+            color = color + simbolos[Math.floor(Math.random() * 16)];
+        }
+    
+        document.body.style.border = color;
+        document.body.style.background = color;
+        document.getElementById("random").innerHTML = color;
+        document.getElementById("clear").innerHTML = "Copiar Color";
+    }
+
 }
